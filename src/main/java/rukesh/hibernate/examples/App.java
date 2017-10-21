cfc34450db09b74ed9be4e1e667b1c1479ec7488
rukesh <rukesh@LAPTOP-NF11M533.home>
2017-10-21 02:41:56 -0400

    Initial Commit

---
+++ src/main/java/rukesh/hibernate/examples/App.java
@@ -0,0 +1,13 @@
+package rukesh.hibernate.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/rukesh/hibernate/examples/Driver.java
@@ -0,0 +1,34 @@
+package rukesh.hibernate.examples;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		//Session session = HibernateUtil.getSessionFactory().openSession();
+		//session.beginTransaction();
+
+		ApplicationUsers appUsers = new ApplicationUsers();
+		appUsers.setUserId("1003");
+		appUsers.setFirstName("Savannah");
+		appUsers.setLastName("Tamrakar");
+		appUsers.setStreet("Earlsfor");
+		appUsers.setCity("Chantilly");
+		appUsers.setState("VA");
+		appUsers.setZip("20152");
+
+		/*session.save(appUsers);    
+
+		session.getTransaction().commit();
+		session.close();
+*/		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("himal");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(appUsers);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+}
